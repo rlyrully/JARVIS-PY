00bd9e299f86d23e0ada494468e15ce26cae7610
rlyrully <saiful.rully96@gmail.com>
2026-07-08 11:30:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,45 @@
 from core.brain import JarvisBrain
 from core.speaker import speak
+from core.listener import listen
 
 
 jarvis = JarvisBrain()
 
 
-speak("Initializing Jarvis System")
-speak("Artificial intelligence online")
+speak(
+    "Initializing Jarvis System"
+)
+
+speak(
+    "Voice interface online"
+)
 
 
 while True:
 
-    command = input("YOU : ")
 
-    if command.lower() == "exit":
-        speak("Shutdown sequence activated")
+    command = listen()
+
+
+    if command == "":
+        continue
+
+
+    if "shutdown" in command.lower():
+
+        speak(
+            "Shutdown sequence activated"
+        )
+
         break
 
 
-    response = jarvis.think(command)
 
-    speak(response)
+    response = jarvis.think(
+        command
+    )
+
+
+    speak(
+        response
+    )
