1dfbd154082deca4180418fcae0cf430840abfc1
rlyrully <saiful.rully96@gmail.com>
2026-07-08 11:34:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,4 @@
-from core.brain import JarvisBrain
-from core.speaker import speak
+from ui.gui import JarvisGUI
 
 
-jarvis = JarvisBrain()
-
-
-def start_text_mode():
-
-    speak(
-        "Jarvis text mode activated"
-    )
-
-
-    while True:
-
-
-        command = input(
-            "YOU : "
-        )
-
-
-        if command.lower() == "exit":
-
-            speak(
-                "Jarvis shutting down"
-            )
-
-            break
-
-
-
-        response = jarvis.think(
-            command
-        )
-
-
-        print(
-            "JARVIS :",
-            response
-        )
-
-
-
-start_text_mode()
+JarvisGUI()
